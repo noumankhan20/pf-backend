be6c48b1462057abb52d7acd72fc742e78de886e
noumankhan20 <noumankhan8097@gmail.com>
2026-01-17 13:21:26 +0530

    refactor: remove Prisma setup and update package.json dependencies
    - Removed Prisma schema file as it is no longer needed.
    - Updated package.json to remove Prisma dependencies and added TypeScript and ts-node as devDependencies.

--- config/db.config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const env= {
-    PORT:process.env.PORT || 7000,
-    DATABASE_URL: process.env.DATABASE_URL
-}
--- config/prisma.client.js
+++
@@ -1,6 +0,0 @@
-// prisma.client.js
-import { PrismaClient } from "@prisma/client";
-
-export const prisma = new PrismaClient({
-  log: ["error"]
-});
