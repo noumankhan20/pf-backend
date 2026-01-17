7452578a79f9f9b3d9706e69129bda53b1c18e4b
noumankhan20 <noumankhan8097@gmail.com>
2026-01-17 13:50:26 +0530

    feat: add phone number field to User model and update server setup with Prisma

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-import express from 'express';
-import cors from "cors";
-import {env} from './config/db.config.js';
+import 'dotenv/config'
+import express from 'express'
+import { PrismaClient } from './generated/prisma/index.js'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const app = express();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
 
-app.use(cors());
-app.use(express.json());
+const prisma = new PrismaClient({ adapter })
 
-app.listen(env.PORT,() =>{
-    console.log(`Server is running on port ${env.PORT}`);
+const app = express()
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  const userCount = await prisma.user.count()
+  res.json(
+    userCount === 0
+      ? 'No users have been added yet.'
+      : `There are ${userCount} users in the database.`
+  )
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
